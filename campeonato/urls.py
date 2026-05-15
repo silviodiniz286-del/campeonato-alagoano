5b5868fac5fbaa38ec0dea172c3262b26dedc2b1
Silvio Diniz <silviodiniz286@gmail.com>
2026-05-15 11:29:43 -0300

    Corrigir imagens dos times

--- campeonato/urls.py
+++ campeonato/urls.py
@@ -1,103 +1,14 @@
-from django.urls import path
-from django.shortcuts import redirect
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
-    # HOME
-    path(
-        '',
-        views.home,
-        name='home'
-    ),
-
-    # ======================
-    # 1ª DIVISÃO
-    # ======================
-
-    path(
-        'rodadas/',
-        lambda request: redirect(
-            'rodada_detalhe',
-            rodada_num=1
-        ),
-        name='rodadas'
-    ),
-
-    path(
-        'rodadas/<int:rodada_num>/',
-        views.rodadas,
-        name='rodada_detalhe'
-    ),
-
-    path(
-        'classificacao/',
-        views.classificacao,
-        name='classificacao'
-    ),
-
-    path(
-        'matamata/',
-        views.matamata,
-        name='matamata'
-    ),
-
-    # ======================
-    # 2ª DIVISÃO
-    # ======================
-
-    path(
-        'segunda-divisao/',
-        lambda request: redirect(
-            'segunda_divisao_rodada',
-            rodada_num=1
-        ),
-        name='segunda_divisao'
-    ),
-
-    path(
-        'segunda-divisao/<int:rodada_num>/',
-        views.segunda_divisao,
-        name='segunda_divisao_rodada'
-    ),
-
-    # ======================
-    # NOTÍCIAS
-    # ======================
-
-    path(
-        'noticias/',
-        views.noticias,
-        name='noticias'
-    ),
-
-    path(
-    'gerar-confrontos/',
-    views.gerar_confrontos_primeira,
-    name='gerar_confrontos'
-),
-
-path(
-    'gerar-confrontos-segunda/',
-    views.gerar_confrontos_segunda,
-    name='gerar_confrontos_segunda'
-),
-
-path(
-    'jogo/<int:jogo_id>/',
-    views.detalhe_jogo,
-    name='detalhe_jogo'
-),
-
-path(
-    'jogo/<int:jogo_id>/simular/',
-    views.simular_partida,
-    name='simular_partida'
-),
-
-path(
-    'gerar-jogadores/',
-    views.gerar_jogadores,
-    name='gerar_jogadores'
-),
+    path('admin/', admin.site.urls),
+    path('', include('campeonato.urls')),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
